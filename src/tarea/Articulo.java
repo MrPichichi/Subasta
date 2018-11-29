354f3d7d1b8529723064147f5805df2837aa6ef2
Francisco <franciscogamboa21@gmail.com>
2018-11-28 22:36:52 -0300

    Initial commit.

---
+++ src/tarea/Articulo.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tarea;
+
+import java.util.HashMap;
+
+public class Articulo {
+    int valor;
+    String nombre;
+    String descripcion;
+    HashMap<Integer, Comprador> pujas = new HashMap<>();
+
+    public int getValor() {
+        return valor;
+    }
+
+    public void setValor(int valor) {
+        this.valor = valor;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+    public void agregarPuja(int puja, Comprador comp){
+        this.pujas.put(puja, comp);
+    }
+    
+}
---
+++ src/tarea/Comprador.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tarea;
+
+/**
+ *
+ * @author FrancsicoGP
+ */
+public class Comprador {
+    String nombre;
+    String login;
+    String contraseña;
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+    
+    
+}
